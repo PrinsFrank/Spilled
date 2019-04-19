b7607794a941e1ac4cf729f77c6bf765e0d38aaa
fprins <25006490+PrinsFrank@users.noreply.github.com>
2019-04-19 17:09:34 +0200

    Moved logic for getting messages so we can do something if we don't have any

--- js/popup/main.js
+++ js/popup/main.js
@@ -4,18 +4,21 @@ Popup = {
     backgroundPageContext: browser.extension.getBackgroundPage().Main,
     messageContainer: document.getElementById('message-container'),
     init: function(){
-        Popup.updateMessageContent();
-    },
-    updateMessageContent: function(){
         Popup.getAllMessages().then(function(messages){
-            messageHtml = '';
-            Object.keys(messages).forEach(function (key) {
-                message = messages[key];
-                messageHtml += '<li id="' + key + '" class="' + message.type + '">' + message.text + '<br><samp>' + message.data + '</samp></li>';
-            });
-
-            Popup.messageContainer.innerHTML = messageHtml;
+            if(Object.getOwnPropertyNames(messages).length){
+                //
+            }
+            Popup.updateMessageContent(messages);
         });
+    },
+    updateMessageContent: function(messages){
+        messageHtml = '';
+        Object.keys(messages).forEach(function (key) {
+            message = messages[key];
+            messageHtml += '<li id="' + key + '" class="' + message.type + '">' + message.text + '<br><samp>' + message.data + '</samp></li>';
+        });
+
+        Popup.messageContainer.innerHTML = messageHtml;
     },
     getMessagesOfType: function(type){
         return browser.tabs.query({currentWindow: true, active: true}).then(function(tab){
