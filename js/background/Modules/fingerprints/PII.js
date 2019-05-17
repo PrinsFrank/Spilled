c1ea04dcd8af8590f9c90f6e9f57d69e0ac53361
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2019-05-17 16:49:44 +0200

    Removed unused feature scaffolding

--- js/background/Modules/fingerprints/PII.js
+++ js/background/Modules/fingerprints/PII.js
@@ -40,36 +40,5 @@ export default {
     firstname: "string",
     lastname: "string",
     streetnumber: "string"
-  },
-  trueNegatives: {
-    cookiesapproved: "bool", // GDPR consent cookie
-    cookieapproved: "bool", // GDPR consent cookie
-    cookies: "bool", // GDPR consent cookie
-    cookie: "bool", // GDPR consent cookie
-    cookiesok: "bool", // GDPR consent cookie
-    cookieok: "bool", // GDPR consent cookie
-    cookiesconsent: "bool", // GDPR consent cookie
-    cookieconsent: "bool", // GDPR consent cookie
-    cookieoptin: "bool", // GDPR consent cookie
-    cookiesoptin: "bool", // GDPR consent cookie
-    cookiefunctional: "bool", // GDPR consent cookie
-    cookiesfunctional: "bool", // GDPR consent cookie
-    cookiemarketing: "bool", // GDPR consent cookie
-    cookiesmarketing: "bool", // GDPR consent cookie
-    cookieanalytics: "bool", // GDPR consent cookie
-    cookiesanalytics: "bool", // GDPR consent cookie
-    cookienecessary: "bool", // GDPR consent cookie
-    cookiesnecessary: "bool", // GDPR consent cookie
-    cookiecategoryfunctional: "bool", // GDPR consent cookie
-    cookiecategorymarketing: "bool", // GDPR consent cookie
-    cookiecategoryanalytics: "bool", // GDPR consent cookie
-    cookiecategorynecessary: "bool", // GDPR consent cookie
-    gdprconsentdate: "date", // GDPR consent cookie
-    trackingcookiesenabled: "bool", // GDPR consent cookie
-    trackingcookieenabled: "bool", // GDPR consent cookie
-    marketingcookiesenabled: "bool", // GDPR consent cookie
-    marketingcookieenabled: "bool", // GDPR consent cookie
-    cookieconsentdismissed: "bool", // GDPR consent cookie
-    acceptallcookies: "bool" // GDPR consent cookie
   }
 };
